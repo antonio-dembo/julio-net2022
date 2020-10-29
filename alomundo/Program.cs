14c35d563a1b1e24a6f04d6c487981fce28ed130
Antonio_Dembo <beboantonio1@hotmail.com>
2020-10-29 08:01:45 -0300

    Template inicial de Console

---
+++ alomundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace alomundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
