928dbcf5a804dd107b656e3a9e7d233d1bc4ced0
Antonio_Dembo <beboantonio1@hotmail.com>
2020-10-30 06:50:04 -0300

    tipo valor em C#

---
+++ tipo_valor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tipo_valor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // tipos imbutidos
+            // a declaração abaixo é a mesma coisa
+            int a = 32;
+
+            // .NET type
+            System.Int32 b = 32;
+
+            Console.WriteLine($"Palavra chave do C#: {a}");
+            Console.WriteLine($"Tipo do .NET: {b}");
+        }
+    }
+}
