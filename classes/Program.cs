ce9e613a43e0129a4faa0a8d0c2fefb478af8256
Antonio_Dembo <beboantonio1@hotmail.com>
2020-11-01 11:17:49 -0300

    executa as classes

--- classes/Program.cs
+++ classes/Program.cs
@@ -6,9 +6,32 @@ namespace classes
     {
         static void Main(String[] args)
         {
-            Cliente c = new Cliente("John Doe", "10/12/1999");
+/*          // Calls the constructor that has no paramenter
+            Person person1 = new Person();
+            Console.WriteLine(person1.Name);
 
-            //Console.WriteLine("Hello World!");
-        }
-    }
-}
+            // Calls the constructor that as one parameter
+            Person person2 = new Person("Sarah Jones");
+            Console.WriteLine(person2.Name);
+
+            // Get the string representation of the person2 instance
+            Console.WriteLine(person2);              // Implicit call to ToString() method
+            Console.WriteLine(person2.ToString());   // explicit call to ToString() method */
+            
+                 
+            Cliente c = new Cliente("John Doe", "10/12/1999");
+            Console.WriteLine(c.LimiteDeCredito);
+            c.LimiteDeCredito = 1000M;
+            c.AumentarLimiteCredito(0.50M);
+            Console.WriteLine($"Limite de crédito é {c.LimiteDeCredito:C}");
+
+            
+
+
+            
+
+                       
+        
+        } // main
+    } // program Class
+} // namespace
