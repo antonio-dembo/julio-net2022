564e346934e794196eb50d672b2b140e3981c0f7
Antonio_Dembo <beboantonio1@hotmail.com>
2020-11-01 08:17:51 -0300

    adiciona mais  membros de classe e exceção

--- classes/Cliente.cs
+++ classes/Cliente.cs
@@ -4,28 +4,56 @@ namespace classes
 {
     public class Cliente
     {
+        // declara campos de instancia (pertence ao meu objeto)
+        // campos: dados do objeto 
         private string nome;
         private DateTime dataDeNascimento;
+        private decimal limiteCredito;
 
-        public Cliente(string umNome, string umaDataIso){
+         public Cliente(string umNome, string umaDataIso){
             nome = umNome;
             dataDeNascimento = DateTime.Parse(umaDataIso);
-
         }
 
-        public Cliente(string umNome){ // dá para criar qantos construtores você quizer.
+        public Cliente(string umNome){ 
             nome = umNome;
             dataDeNascimento = DateTime.Now;
+        }
 
+        public void AumentarLimiteCredito(decimal porcentagem){
+            decimal aumento = porcentagem * limiteCredito;
+            limiteCredito += aumento;
+
+        }
+
+       
+        public decimal LimiteDeCredito {
+            // cria uma "Propriedade" para o campo "limiteCredito", para expor seus dados
+            // Não é um método porque não tem passagem de parámetros
+            // pode ser de leitura(get) ou escrita(set)
+            // Pode ter qualquer nome
+            // value: proriedade implicita do C# que recebe o nome que deve ser atribuido ao limiteCredito
+            get{
+                return limiteCredito;
+            }
+            set{
+                // limite de crédito não pode ser negativo: validação
+                // uso de exceção (Exception)
+                if (value < 0){
+                    // Gerar uma exceção
+                    throw new ArgumentOutOfRangeException($"{nameof(value)} Deve ser maior ou igual a 0)");
+                }
+                limiteCredito = value;
+            }
         }
 
 
 
 
 
-    }
 
-}
+    } // Cliente
+}// namespace
 
 
 
