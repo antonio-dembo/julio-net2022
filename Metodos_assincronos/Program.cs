14845a004486f60bc99ba798bf90e530721c3099
Antonio_Dembo <beboantonio1@hotmail.com>
2020-11-05 07:57:34 -0300

    consumo do web service reqres.in

---
+++ Metodos_assincronos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Metodos_assincronos
+{
+    class Program
+    {
+        static readonly HttpClient cliente = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            try{
+                var resposta = await cliente.GetAsync("https://reqres.in/api/users/2");
+                if(resposta.IsSuccessStatusCode){
+                    string dados = await resposta.Content.ReadAsStringAsync();
+                    Console.WriteLine(dados);
+                }
+                
+            }
+            catch(HttpRequestException e){
+                Console.WriteLine("Falha do consumo do Web Service: ");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
