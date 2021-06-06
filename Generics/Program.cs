d8afd492b137c4c22277148312c9511fc294feba
Antonio Dembo <beboantonio1@hotmail.com>
2021-06-06 14:44:07 -0300

    test value type constraint

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -7,19 +7,9 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            var book = new Book { Isbn= 1111, Title= "C# Advanced"};
-
-            // var Books = new BookList();
-            // Books.Add(book);
-
-            var numbers = new GenericList<int>();
-            numbers.Add(10);
-
-            var books = new GenericList<Book>();
-            books.Add(new Book());
-            
-            var dictionary = new GenericDictonary<string, Book>();
-            dictionary.Add("123", new Book());
+            var number = new Nullable<int>(5);
+            Console.WriteLine("Has value ?" + number.HasValue);
+            Console.WriteLine("Value= " + number.GetValueOrDefault());
 
         }
     }
