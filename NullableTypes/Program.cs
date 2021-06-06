1e0e1c1b2ee8f9721581d0a9c7bcb9aecfffba38
Antonio Dembo <beboantonio1@hotmail.com>
2021-06-06 13:07:41 -0300

    first commit

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? n = null;
+            int m = n.GetValueOrDefault();
+
+            Console.WriteLine(m);
+
+            //dynamic num1 = 5; // postpone the type resolution at runtime
+            var num2 = 5; // checed at compile time
+
+        }
+    }
+}
