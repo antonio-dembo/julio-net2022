6c667b9592786e1c6feb50c043fc57666425858d
Antonio Dembo <beboantonio1@hotmail.com>
2021-06-06 13:09:41 -0300

    add try-catch structure

--- tipo_valor/Program.cs
+++ tipo_valor/Program.cs
@@ -8,13 +8,28 @@ namespace tipo_valor
         {
             // tipos imbutidos
             // a declaração abaixo é a mesma coisa
-            int a = 32;
+            // int a = 32;
 
-            // .NET type
-            System.Int32 b = 32;
+            // // .NET type
+            // System.Int32 b = 32;
 
-            Console.WriteLine($"Palavra chave do C#: {a}");
-            Console.WriteLine($"Tipo do .NET: {b}");
+            // Console.WriteLine($"Palavra chave do C#: {a}");
+            // Console.WriteLine($"Tipo do .NET: {b}");
+
+            if ((decimal) Math.Pow(2, 5350) > decimal.MaxValue){
+                throw new OverflowException();
+            }
+
+            // try{
+            //     decimal longNumber = (decimal) Math.Pow(2, 5350) ;
+            //     Console.WriteLine(longNumber % 7);
+            // }
+            // catch(OverflowException e){
+            //     Console.WriteLine(e.Message);
+            // }
+
+            
+            
         }
     }
 }
