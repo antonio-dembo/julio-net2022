b31cb3bf806c3fefc7a2906a5e3b68b24964112f
Antonio Dembo <beboantonio1@hotmail.com>
2021-06-06 14:05:57 -0300

    add contraint at method level

--- Generics/Utilities.cs
+++ Generics/Utilities.cs
@@ -9,7 +9,7 @@ namespace Generics
           return a > b ? a : b ;
         }
 
-        public T Max<T> (T a, T b)
+        public T Max<T> (T a, T b) where T : IComparable
         {
           // at this time the compiler doesn't know the type of T.
           // so it cannot apply comparison with a and b.
@@ -17,7 +17,7 @@ namespace Generics
           // Solution: Use contraints: 
           // we want to assume that both a and be implements the IComparable interface, which provides a method called
           // CompareTo(). with that we can compare this two objects. that's a use case where we need to appply a constraints
-          return a > b ? a : b ; 
+          return a.CompareTo(b) > 0 ? a : b ; 
         }
     }
 }
